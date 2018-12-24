c6a3aaf1338d76afeaf1590233c5d90af099c667
NetworkRanger <17346503142@163.com>
2018-12-24 23:40:34 +0800

    ADD: 完成至第4章

--- chapter04/bayes.py
+++ chapter04/bayes.py
@@ -141,6 +141,7 @@ def spamTest():
             errorCount += 1
     print 'the error rate is: ', float(errorCount) / len(testSet)
 
+
 # RSS源分类器及高频词去除函数
 def calcMostFreq(vocabList, fullText):
     # 计算出现频率
@@ -151,8 +152,61 @@ def calcMostFreq(vocabList, fullText):
     sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedFreq[:30]
 
+
 def localWords(feed1, feed0):
     import feedparser
     docList = []
     classList = []
     fullText = []
+    minLen = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(minLen):
+        # 每次访问一条RSS源
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    # 去掉出现次数最高的那些词
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList, fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList: vocabList.remove(pairW[0])
+    trainingSet = range(2 * minLen)
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(bagOfWord2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = bagOfWord2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print 'the error rate is: ', float(errorCount) / len(testSet)
+    return vocabList, p0V, p1V
+
+# 分析数据：显示地域相关的用词
+def getTopWords(ny,sf):
+    import operator
+    vocabList, p0V, p1V = localWords(ny,sf)
+    topNY = []
+    topSF = []
+    for i in range(len(p0V)):
+        if p0V[i] >= -6.0: topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] >= -6.0: topNY.append(vocabList[i], p1V[i])
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print 'SF**SF**SF**SF**SF**SF**SF**SF**SF**SF***SF**SF**SF**SF**SF'
+    for item in sortedSF:
+        print item[0]
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    print 'NY**NY**NY**NY**NY**NY**NY**NY**NY**NY***NY**NY**NY**NY**NY'
